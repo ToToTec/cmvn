cb600746850c6e5e3089d3020e4b291bd85e2adc
Tobias Roeser <tobias.roeser@tototec.de>
2011-04-10 18:38:41 +0000

    Improved make-source-release.sh.

--- make-source-release.sh
+++ make-source-release.sh
@@ -6,8 +6,12 @@ CMVN_DIR=${RELEASE_DIR}/${CMVN_PACK}
 
 mkdir -p ${RELEASE_DIR}
 
+rm -r ${CMVN_DIR}
+rm -r ${CMVN_DIR}.zip
+
 svn export . ${CMVN_DIR}
 
 rm -r ${CMVN_DIR}/releases
+rm ${CMVN_DIR}/make-source-release.sh
 
 ( cd ${RELEASE_DIR} && zip -r ${CMVN_PACK}.zip ${CMVN_PACK} )

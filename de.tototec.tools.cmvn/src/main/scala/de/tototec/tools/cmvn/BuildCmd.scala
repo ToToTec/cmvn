8b2ea598b50b190a6b6b01b58124cc87b29ab7ff
Tobias Roeser <tobias.roeser@tototec.de>
2012-03-02 19:52:34 +0000

    Code formatted.

--- de.tototec.tools.cmvn/src/main/scala/de/tototec/tools/cmvn/BuildCmd.scala
+++ de.tototec.tools.cmvn/src/main/scala/de/tototec/tools/cmvn/BuildCmd.scala
@@ -4,14 +4,16 @@ import de.tototec.cmdoption.CmdCommand
 import de.tototec.cmdoption.CmdCommand
 import de.tototec.cmdoption.CmdOption
 
-@CmdCommand(names = Array("--build"), description = "Build the project and all sub projects (Requires a configured project)")
+@CmdCommand(names = Array("--build"),
+  description = "Build the project and all sub projects (default command, when non given, requires a configured project)")
 class BuildCmd extends HelpAwareCmd {
 
   @CmdOption(names = Array("--build-from-root"),
     description = "Run Maven from root project with additional '-pl <current-project>'")
   var buildFromRoot = false
 
-  @CmdOption(args = Array("mvnarg"), description = "Arguments delegated to the Maven build tool.", minCount = 1, maxCount = -1)
+  @CmdOption(args = Array("mvnarg"), minCount = 1, maxCount = -1,
+    description = "Arguments delegated to the Maven build tool.")
   var mvnArgs = new java.util.LinkedList[String]
 
 }
